d5dbc91c7db433fbdb7d56d5c7cdb4ed87a0c8e7
bbtgnn <bbt.gnn@gmail.com>
2022-01-13 21:04:56 +0100

    delete everything and redo

---
+++ getContentfulEnvironment.js
@@ -0,0 +1,11 @@
+// const contentfulManagement = require("contentful-management");
+
+// module.exports = function () {
+//   const contentfulClient = contentfulManagement.createClient({
+//     accessToken: "qkYzOuZ3MedZ5E0A2OWWJTtTRjGORcHWO2cWS6It5Lo",
+//   });
+
+//   return contentfulClient
+//     .getSpace("9cbraljlhlcw")
+//     .then((space) => space.getEnvironment("v2"));
+// };
